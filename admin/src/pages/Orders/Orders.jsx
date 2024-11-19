b70d1bb9f8854e687592d3fa9fb973a7e958998a
Soumyajit Manna <122715874+soumyajit-manna@users.noreply.github.com>
2024-11-19 23:30:01 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Orders.css'
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import { assets, url, } from '../../assets/assets';
+import { assets} from '../../assets/assets';
 
 const Order = () => {
 
